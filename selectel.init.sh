4f88565019958efa7d15d4614aa51dd11ba25c4b
romanTechDev <83161900+romanTechDev@users.noreply.github.com>
2025-02-07 10:38:44 +0300

    Delete selectel.init.sh

--- selectel.init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt update 
-apt install ansible git htop -y
-timedatectl set-timezone Europe/Moscow
-useradd -m -p  -s /bin/bash ansible
-mkdir /home/ansible/.ssh
-echo "" >> /home/ansible/.ssh/authorized_keys
-sudo echo "ansible ALL=(ALL) ALL" >> /etc/sudoers
