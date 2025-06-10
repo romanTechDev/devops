9b08dfe7a69b9c978cb72b96fe79731fc28c77e5
romanTechDev <romantechdev@gmail.com>
2025-06-10 14:10:38 +0300

    add webhook workflow

--- docker/docker_composes/certbot/docker_run.sh
+++ docker/docker_composes/certbot/docker_run.sh
@@ -1,3 +1,5 @@
+mkdir -p /var/www/letsencrypt/.well-known/acme-challenge
+
 docker run -it --rm \
   -v /etc/ssl/certs:/etc/letsencrypt \
   -v /var/www/letsencrypt:/var/www/letsencrypt \
@@ -6,5 +8,26 @@ docker run -it --rm \
   --webroot-path=/var/www/letsencrypt \
   -d <domain>
 
-mkdir -p /var/www/letsencrypt/.well-known/acme-challenge
 
+docker run -it --rm \
+  -v /etc/ssl/certs:/etc/letsencrypt \
+  -v /var/www/letsencrypt:/var/www/letsencrypt \
+  certbot/certbot certonly \
+  --manual \
+  --preferred-challenges dns \
+  --email rpetrov@moymir.ru \
+  --server https://acme-v02.api.letsencrypt.org/directory \
+  -d "*.hsr24.ru" \
+  -d "hsr24.ru"
+  
+  backend.postgre_rr.hsr24.ru
+  rpetrov@moymir.ru
+
+docker run -it --rm \
+  certbot/certbot certonly \
+  --manual \
+  --preferred-challenges dns \
+  --email rpetrov@moymir.ru \
+  --server https://acme-v02.api.letsencrypt.org/directory \
+  -d "*.hsr24.ru" \
+  -d "hsr24.ru"
